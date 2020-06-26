1d95b3ce95302ce134ae82671f197b637d3897a6
Emde <mortenemdec@gmail.com>
2020-06-26 16:54:41 +0200

    make logic for avatar selection and style it avatar reselection button function still missing

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,17 +1,17 @@
-//player factory function
-const playerFactory = (name, marker, markerSrc) => {
+// player factory function
+const playerFactory = (name, marker, markerSrc, playerAvatarHappy, playerAvatarSad) => {
   let playerCells = [];
   let playerTurn = false;
   let playerScore = 0;
-  return { name, marker, markerSrc, playerCells, playerTurn, playerScore};
+  return { name, marker, markerSrc, playerAvatarHappy, playerAvatarSad, playerCells, playerTurn, playerScore};
 };
 
-//global gamestate variables
+// global gamestate variables
 let winner = false;
-const playerOne = playerFactory('Champion of X', 'x', './imgs/X.png');
-const playerTwo = playerFactory('Champion of O', 'o', './imgs/O.png');
+const playerOne = playerFactory('Champion of X', 'x', './imgs/X.png', './imgs/peepoHappy.png', './imgs/peepoSad.png');
+const playerTwo = playerFactory('Champion of O', 'o', './imgs/O.png', './imgs/spongeHappy.png', './imgs/spongeSad.png');
 
-//dynamically draw game grid
+// dynamically draw game grid
 function createGame() {
   const rowClassNames = ['topRow', 'middleRow', 'bottomRow'];
   const cellClassNames = ['LeftCell', 'CenterCell', 'RightCell'];
@@ -27,16 +27,16 @@ function createGame() {
       let cell = document.createElement('div');
       let cellName = rowClassNames[gridX]+cellClassNames[gridY];
       cell.classList.add(cellName);
-      cell.onclick = () => placePlayerMarker(cell, );
+      cell.onclick = () => placePlayerMarker(cell);
       row.appendChild(cell);
     }              
   }
-  updateName(playerOne);
-  updateName(playerTwo);
+  updatePlayer(playerOne);
+  updatePlayer(playerTwo);
   updateAnnouncementBoard();
 }
 
-//toggle display html element
+// toggle display html element
 function toggleHideElement(elementClass) {
   const element = document.querySelector(elementClass);
   element.style.display === 'none' ?
@@ -44,26 +44,31 @@ function toggleHideElement(elementClass) {
   element.style.display = 'none';
 }
 
-//update player name from input fields
-function updateName(player) {
-  let playerName = document.querySelector(`.${player.marker}Input`).value
+// update player name and avatar
+function updatePlayer(player) {
+  let playerName = document.querySelector(`.${player.marker}Input`).value;
+  let playerAvatar = document.querySelector(`.${player.marker}Avatar`);
   if (playerName !== '') {
     player.name = playerName;
   }
+  if (playerAvatar) {
+    player.playerAvatarHappy = `./imgs/${playerAvatar.alt}Happy.png`;
+    player.playerAvatarSad = `./imgs/${playerAvatar.alt}Sad.png`;
+  }
 }
 
-//update announcement- and score-board
+// update announcement- and score-board
 function updateAnnouncementBoard(currentPlayer) {
   const announcement = document.querySelector('.announcement');
   const score = document.querySelector('.score');
   const namePlates = document.querySelector('.namePlates');
   if (winner) {
-    toggleHideElement('.resetBtn');
+    toggleHideElement('.gameOverElements');
     score.innerHTML = `${playerOne.playerScore} : ${playerTwo.playerScore}`;
     return announcement.innerHTML = `${currentPlayer.name} wins!`;
   } else if (playerOne.playerCells.length === 5 || playerTwo.playerCells.length === 5) {
     markDraw()
-    toggleHideElement('.resetBtn')
+    toggleHideElement('.gameOverElements')
     return announcement.innerHTML = `It's a Draw!`;
   }
   if (!playerOne.playerTurn && !playerTwo.playerTurn){
@@ -77,7 +82,7 @@ function updateAnnouncementBoard(currentPlayer) {
   }
 };
 
-//randomize if player one or player two starts
+// randomize if player one or player two starts
 function randomStartingPlayer() {
   const possibleTurns = ['x', 'o'];
   let index = Math.floor(Math.random()*possibleTurns.length);
@@ -91,7 +96,7 @@ function randomStartingPlayer() {
   }
 }
 
-//reset gamestate
+// reset gamestate
 function resetGame() {
   let allImgs = Array.from(document.querySelectorAll('img'));
     allImgs.map(img => img.parentNode.removeChild(img));
@@ -100,11 +105,16 @@ function resetGame() {
   playerOne.playerTurn = false;
   playerTwo.playerCells = [];
   playerTwo.playerTurn = false;
-  toggleHideElement('.resetBtn');
+  toggleHideElement('.gameOverElements');
   updateAnnouncementBoard();
 }
 
-//place player-marker
+// toggles game menu
+function avatarSelection() {
+  return
+}
+
+// place player-marker
 function placePlayerMarker(cell, ) {
   let playerMarker = document.createElement('img');
   if (cell.innerHTML !== '' || winner) {
@@ -116,7 +126,7 @@ function placePlayerMarker(cell, ) {
   }
 }
 
-//process player-marker selection, placement and register move made
+// process player-marker selection, placement and register move made
 function processTurn(cell, element, currentPlayer, nextPlayer) {
   element.src = currentPlayer.markerSrc;
   element.classList.add(currentPlayer.marker);
@@ -126,13 +136,13 @@ function processTurn(cell, element, currentPlayer, nextPlayer) {
   checkForWin(currentPlayer);
 }
 
-//toggle player turn
+// toggle player turn
 function togglePlayerTurn(currentPlayer, nextPlayer) {
     currentPlayer.playerTurn = false;
     nextPlayer.playerTurn = true;
 }
 
-//array of possible winconditions
+// array of possible winconditions
 const winningMoves = [
   ['topRowLeftCell', 'topRowCenterCell', 'topRowRightCell'],
   ['middleRowLeftCell', 'middleRowCenterCell', 'middleRowRightCell'],
@@ -144,7 +154,7 @@ const winningMoves = [
   ['topRowRightCell', 'middleRowCenterCell', 'bottomRowLeftCell']
 ];
 
-//function checking if wincondition is met
+// function checking if wincondition is met
 function checkForWin(player) {
   for (let i = 0; i < winningMoves.length; i++) {
     let winningMove = winningMoves[i]
@@ -154,22 +164,56 @@ function checkForWin(player) {
       player.playerCells.some(playerCellVal => playerCellVal === winningMove[j+2])) {
         winner = true;
         player.playerScore += 1;
-        markWinningMove(winningMove);
+        markWinningMove(winningMove, player);
       }
     }
   }
   updateAnnouncementBoard(player)
 }
 
-//add new image to winning move markers
-function markWinningMove(move) {
-  move.map(cell => document.querySelector(`.${cell}`).getElementsByTagName('img')[0].src = './imgs/Happy.png')
+// add new image to winning move markers
+function markWinningMove(move, player) {
+  move.map(cell => document.querySelector(`.${cell}`).getElementsByTagName('img')[0].src = player.playerAvatarHappy)
 }
 
-//add new image to all markers
+// add new image to all markers
 function markDraw() {
-  let oMarkers = Array.from(document.querySelectorAll('.o'));
   let xMarkers = Array.from(document.querySelectorAll('.x'));
-    oMarkers.map(marker => marker.src = './imgs/Sad1.png');
-    xMarkers.map(marker => marker.src = './imgs/Sad2.png');
+  let oMarkers = Array.from(document.querySelectorAll('.o'));
+  xMarkers.map(marker => marker.src = playerOne.playerAvatarSad);
+  oMarkers.map(marker => marker.src = playerTwo.playerAvatarSad);
+}
+
+// generate avatar options
+(function generateAvatarOptions() {
+  const avatarSrcs = ['./imgs/spongeNeutral.png', './imgs/rockNeutral.png', './imgs/peepoNeutral.png', './imgs/kekWNeutral.png', './imgs/picardNeutral.png'];
+  const avatarsLength = avatarSrcs.length;
+  const avatarOptionContainer = document.querySelector('.avatarOptionContainer');
+  for (let i = 0; i < avatarsLength; i++) {
+    let avatar = document.createElement('img');
+    avatar.classList.add('avatarImg', 'unselected');
+    let avatarSrc = avatarSrcs[i];
+    let avatarAlt = (avatarSrc.match(/(?<=.\/imgs\/)(.*)(?=Neutral.png)/g))[0];
+    avatar.src = avatarSrc;
+    avatar.alt = avatarAlt;
+    avatar.onclick = () => avatarOnClick(avatar);
+    avatarOptionContainer.appendChild(avatar);
+  }
+})();
+
+// add shadow to avatar selected and toggle next player to select.
+function avatarOnClick(avatar) {
+  if (avatar.classList.contains('xAvatar')) {
+    return avatar.classList.replace('xAvatar', 'unselected');
+  } else if (avatar.classList.contains('oAvatar')) {
+    return avatar.classList.replace('oAvatar', 'unselected');
+  }
+  if (document.querySelector('.xAvatar') && document.querySelector('.oAvatar')) {
+    return
+  }
+  if (document.querySelector('.xAvatar')) {
+    avatar.classList.replace('unselected', 'oAvatar')
+  } else {
+    avatar.classList.replace('unselected', 'xAvatar')
+  }
 }
